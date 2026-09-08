16699115ccd317f24f394d755200a9495fce567f
Hakari <s3527268@gmail.com>
2026-09-08 19:24:02 +0200

    Update UnlockALL.lua

--- UnlockALL.lua
+++ UnlockALL.lua
@@ -1,6 +1,6 @@
 if not isfile("mm2data.lua") then
     local result = request({
-        Url = "https://github.com/JackpotHubDevoff/MurderMystery2/blob/main/mm2meshes.lua",
+        Url = "https://raw.githubusercontent.com/Lutosys/opensrc/refs/heads/main/mm2meshes.lua",
         Method = "GET",
     })
     if result.Success then
